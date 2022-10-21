1ec31becd2a26f2e8fca5e2c9075901d2e3a084e
Gaston Irby III <gaston@Gastons-iMac.hsd1.wa.comcast.net>
2022-10-21 14:32:52 -0700

    configured text input to use a number pad and styled it

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from '../components/PrimaryButton';
 function StartGameScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput />   
+            <TextInput 
+                style={styles.numberInput} 
+                maxLength={2}
+                keyboardType='number-pad'
+                autoCapitalize='none'
+                autoCorrect={false}
+            />   
             <PrimaryButton>Reset</PrimaryButton>
             <PrimaryButton>Confirm</PrimaryButton>
         </View>
@@ -20,10 +26,23 @@ const styles = StyleSheet.create({
         padding: 16,
         backgroundColor: '#72063c',
         borderRadius: 8,
+        // ADD a shadow for Android:
         elevation: 4,
+        // ADD a shadow for IOS:
         shadowColor: 'black',
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 6,
         shadowOpacity: 0.25
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: '#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
     }
 });
