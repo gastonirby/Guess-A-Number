d5738a3bd98c61aa73fb9a504bcec0ac0513d62f
Gaston Irby III <gaston.irby@gmail.com>
2022-10-24 16:04:50 -0700

    added style properties for responsiveness

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -19,5 +19,8 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: Colors.accent500,
         padding: 12,
+        maxWidth: '80%',
+        minWidth: '60%',
+        width: 300,
     }
 })
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -110,7 +110,8 @@ export default GameScreen;
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        padding: 12
+        padding: 12,
+        alignItems: 'center',
     },
     instructionText: {
         marginBottom: 12,
