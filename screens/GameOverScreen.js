e1e31df81518f0420d4f39f7673ae2ae3222cc9b
Gaston Irby III <gaston@Gastons-iMac.hsd1.wa.comcast.net>
2022-10-24 10:02:49 -0700

    added game over image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,45 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
 
 import Instruction from '../components/ui/Instruction';
+import Title from '../components/ui/Title';
+import Colors from '../components/constants/colors';
 
 function GameOverScreen() {
     return (
-        <Instruction>Game Over!</Instruction>
+        <View style={styles.rootContainer} >
+            <Title>Game Over!</Title>
+            <View style={styles.imageContainer} >
+                <Image 
+                    style={styles.image}
+                    source={require('../assets/images/success.png')} 
+                />
+            </View>
+        </View>
     )
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: Colors.primary800,
+        overflow: 'hidden',
+        margin: 36,
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+});
